2309d8603db4534eec0826a2459c0c3f88f7e901
austinxduong <axduong2@gmail.com>
2021-09-15 02:03:07 -0700

    loading a module, part 1

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ helloWorld('returning first parameter');
 // run node on terminal, and pass file name (app.js) as an argument
 // (ie. node app.js)
 // What happends under the hood? Node passes app.js to googles' v8 engine for execution (a c++ program that uses v8)
+
+// now, lets load the module we just create (login.js)
+// we use the 'require' function to load modules
+// it takes in a single argument, which is the path of the module for login.js
+// require('./login')
+
+// now lets just turn it into a variable
+const login = require('./login');
+// console.log(login);
+login.log('hi its austi');
+
+
+// in the terminal, we should see a returned object for our login module. verified as a console
+// now, lets call the login module in app.js
+// we now see "hi it's austi" in the terminal 
--- globalObject.js
+++ globalObject.js
@@ -5,5 +5,9 @@ console.log("1. we have Global Object in node. We don\'t have window object")
 console.log("2. what is a module?")
 
 console.log("3. piece of code that contains functionality. functionality that can also be accessed, and easily pluggable. can be easily deployed. this is most objective description as I can put it... ")
+// oh, every file is a module essentially
 
 console.log(module)
+
+// now lets create a 'module'
+// see login.js
