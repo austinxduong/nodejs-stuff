6fcb2cdf5068f150e86fb61a483396b66bf61712
austinxduong <axduong2@gmail.com>
2021-09-14 21:54:30 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("0. To check your node version, run node -v in terminal")
+
+console.log("1. Hello World, from austi d");
+
+console.log("2. What is node.js?")
+
+console.log("3. Runtime environment for executing javascript code (in a server or local development environment instead of browser")
+
+console.log("4. In Terminal type node app.js. Console log message nows shows in Terminal")
+
+console.log("5. Type nodemon apps.js")
+
+console.log("6. What is nodemon?")
+
+console.log("7. A utility that monitors for changes, and automatically restarts the server for you. Good for productivity")
---
+++ nodejsFacts.js
@@ -0,0 +1,17 @@
+console.log("1. Node.js is not a programming language, & not a framework. It is a runtime environment")
+
+console.log("2. Node.js has an (non blocking) asynchronous nature/architecture")
+
+console.log("3. What is asynchronous? Multiple events can be executed at the sametime and/or independently")
+
+console.log("4. Synchronous architecture is opposite. One event must finish executing, before the next event can be ran/executed")
+
+console.log("5. Node.js is asynchronous by default, while other frameworks are synchronous out of the box")
+
+console.log("6. Node.js is reccomended for data intensive/highly scalable apps")
+
+console.log("7. Node.js can handle multiple requests, just by a single thread")
+
+
+
+
