ab631d64493aa0f7d9b2dab7593f0fcc5af4194e
austinxduong <axduong2@gmail.com>
2021-09-15 00:30:44 -0700

    reading about globla objects+modules

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ helloWorld('returning first parameter');
 
 // run node on terminal, and pass file name (app.js) as an argument
 // (ie. node app.js)
-// What happends under the hood? Node passes app.js to googles' v8 engine for execution
+// What happends under the hood? Node passes app.js to googles' v8 engine for execution (a c++ program that uses v8)
---
+++ globalObject.js
@@ -0,0 +1,9 @@
+console.log(); //<-- we call this a "Global Object". we can access this anywhere, in any files
+
+console.log("1. we have Global Object in node. We don\'t have window object")
+
+console.log("2. what is a module?")
+
+console.log("3. piece of code that contains functionality. functionality that can also be accessed, and easily pluggable. can be easily deployed. this is most objective description as I can put it... ")
+
+console.log(module)
