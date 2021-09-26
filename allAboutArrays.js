7e7515e8ce139320b2e86f3caa1bf5add2d1671b
austinxduong <axduong2@gmail.com>
2021-09-25 18:19:46 -0700

    all about arrays

---
+++ allAboutArrays.js
@@ -0,0 +1,3 @@
+// purpose: general content hub, about arrays. because we love arrays
+// whats an array?
+// an array is a list of values, that store data. we can add/update/change, etc these data(s) in many flavors
