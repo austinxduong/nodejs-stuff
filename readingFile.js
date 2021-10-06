34c1b8a93d8b58b571d5a3c4867b18c1a42af7f7
austinxduong <axduong2@gmail.com>
2021-10-05 17:50:24 -0700

    reading a file async/await style

--- readingFile.js
+++ readingFile.js
@@ -1,7 +1,8 @@
 //here, we are demonstrating how to read a .text file
 // first, we import the prebuilt 'fs' module that comes with node.js
-const fs = require('fs')
-const fsPromises = fs.promises;
+const fs = require('fs') // Global Object
+const fsPromises = fs.promises; // Global Object
+
 
 
 
@@ -10,10 +11,10 @@ fs.readFile('helloAustiWorld.txt', (err, data) => {
         console.error(err);
         return;
     }
-    console.log('reading a file using callback function', data.toString()); //<-- to grab "Hello World" in the .txt file, we need a ".toString()". it now shows the console log.
+    console.log('reading a file using callback function style', data.toString()); //<-- to grab "Hello World" in the .txt file, we need a ".toString()". it now shows the console log.
 });
-// to see the asynchronus nature of node lets create another console.log 
-console.log('testing asynchronous nature of node');
+// // to see the asynchronus nature of node lets create another console.log 
+// console.log('testing asynchronous nature of node');
 
 // under the hood asynchornous explanation: node reads the file first on line 5
 // while node is busy reading the file first, its going to move on, & execute our console log on line 13.
@@ -25,15 +26,38 @@ console.log('testing asynchronous nature of node');
 // a promise makes handling asynchronus operations easier, a placeholder for a task that hasn't finished yet, and will be executed later (or in the future). Hence "promise".
 fs.promises.readFile("helloAustiWorld.txt")
 .then(function(result) {
-    console.log("reading a file promise"+result);
+    console.log("reading a file promise style"+result);
 })
 .catch(function(error){
     console.log(error);
 })
 
-// now... lets async await this from the promise syntax 
-// async/await hides the the operations of the "promise" syntax under the hood
-// in our modern era, it is much cleaner and easier code to read and much more teachable and less "gatekeepy", although important to understand at the core "under the hood"
+// async function readingAFileIdkWhatImDiong() {
+//     try {
+//         const readThisData = await fs.readFile('helloAustiWorld.txt');
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// } //total fail, it didn't work lol
 
+// async function readingThisFile() {
+//     const fsPromises = require('fs').promises;
+//     const data = await fsPromises.readFile('helloAustiWorld.txt')
+//         .catch((err) => console.error ('file not read', err));
+// }
+// console.log(data.toString());
 
+async function readFile(readingAustiFile) {
 
+    const fs = require('fs').promises;
+
+    try {
+        const data = await fs.readFile(readingAustiFile);
+        console.log("reading a file async/await style", data.toString());
+    } catch (error) {
+        console.error(`Error Reading File: ${error.message}`);
+    }
+}
+
+readFile('helloAustiWorld.txt');
