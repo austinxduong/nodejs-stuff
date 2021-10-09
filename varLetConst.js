95cd931354099fc53db3b259d1f7470e619b11d2
austinxduong <axduong2@gmail.com>
2021-10-08 21:45:21 -0700

    hay

--- varLetConst.js
+++ varLetConst.js
@@ -1,2 +1,3 @@
 // purpose: explore the multi-faceted behaviors + see the uniqueness, if we ever want to call/declare a variable(s) using either "Var / Let / Const"
 // what we'll learn: the various oppurtunities/tricks/tips, and/or constraints when declaring variables, with a label(s); Var, Let, Const
+// guess i should start now
