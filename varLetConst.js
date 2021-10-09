47cd135f6bba5048fccd994c2144d43d860d671a
austinxduong <axduong2@gmail.com>
2021-10-08 22:23:55 -0700

    "var" can b reassigned, using same label indicator

--- varLetConst.js
+++ varLetConst.js
@@ -1,3 +1,14 @@
 // purpose: explore the multi-faceted behaviors + see the uniqueness, if we ever want to call/declare a variable(s) using either "Var / Let / Const"
 // what we'll learn: the various oppurtunities/tricks/tips, and/or constraints when declaring variables, with a label(s); Var, Let, Const
 // guess i should start now
+
+// lets you reassign value, using same label to define a variable. when using "var"
+var thisIsCalledALabel = "playing"
+
+var thisisCalledALabel = "orrr should it be this?"
+
+//lets console.log this out
+console.log(thisisCalledALabel); //<-- terminate this statement with a semi :) then, run server from terminal
+
+// orrr should it be this? <-- this is returned
+// tested. so yes. variables can be reassigned, when using "var"
