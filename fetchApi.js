61f4bfdb1d7a848b66b2c65df081650c49a89eff
austinxduong <axduong2@gmail.com>
2022-02-20 19:12:13 -0800

    playing in sandbox on a sunday

---
+++ fetchApi.js
@@ -0,0 +1,9 @@
+// export const fetchApi = async () => {
+//     const res = await fetch('https://whispering-citadel-46770.herokuapp.com/api/v1/planets');
+//     const json = await res.json();
+
+//     return json;;
+// }
+
+
+console.log("austi moved to the console")
